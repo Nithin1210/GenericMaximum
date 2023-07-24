a8c3b1342a5aebcf5c37bb8002f41ef180e3b70c
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-24 15:51:18 +0530

    [Nithin] Add : Updation of Maxinterger

---
+++ FindMaximum/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class Operation
+    {
+        public void FindMaxInteger(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+                Console.WriteLine("a is maximum");
+            if (b >= c && b >= a)
+                Console.WriteLine("b is maximum");
+            if (c >= a && c >= b)
+                Console.WriteLine("c is maximum");
+        }
+
+    }
+}
---
+++ FindMaximum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FindMaximum
+{
+     class Program
+    {
+         static void Main(string[] args)
+        {
+            Operation max = new Operation();
+            max.FindMaxInteger(1, 3, 5);
+
+        }
+    }
+
+}
