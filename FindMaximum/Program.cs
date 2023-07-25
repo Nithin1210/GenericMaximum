92a3fcb21ab13b4ac4dab76bb67c7be2b389ab39
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-25 22:13:12 +0530

    [Nithin] Add: Updation of UC5-PrintMax using Generic

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -10,10 +10,10 @@ namespace FindMaximum
         {
             Console.WriteLine("!! Finding the maximum !!");
             Generic<int> max1 = new Generic<int>();
-            max1.FindMax(1, 2, 3);
+            max1.FindMax(11, 22, 33);
 
             Generic<float> max2 = new Generic<float>();
-            max2.FindMax(2.5f, 9.9f, 11.2f);
+            max2.FindMax(11.5f, 22.5f, 33.5f);
 
             Generic<string> max3 = new Generic<string>();
             max3.FindMax("spiderman", "antman", "batman");
