6db5637cb542e18c182be7b659048517e4106177
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-25 15:49:58 +0530

    [Nithin] Add: Updation of MultipleParameters

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -9,6 +9,7 @@ namespace FindMaximum
         {
             string[] a = { "mn", "qa", "eg", "zx", "as", "qw" };
             Operation<string> operation = new Operation<string>(a);
+            Console.WriteLine("string elements are :: mn, qa, eg, zx, as, qw ");
             Console.WriteLine("Maximum value is : " + operation.FindMax());
         }
     }
